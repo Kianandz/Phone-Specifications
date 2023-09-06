86c706b180a4b2bf649306fb13e5cb61a8685973
Kianandz <115959488+Kianandz@users.noreply.github.com>
2023-09-06 15:43:49 +0700

    Add files via upload

--- src/Component/Fragment/Sidebar.jsx
+++ src/Component/Fragment/Sidebar.jsx
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Title from "../Element/Title";
 import Text from "../Element/Text";
 import IconFA from "../Element/IconFA";
+import IconGoogle from "../Element/IconGoogle";
+import "../../assets/Css/Sidebar.css";
 
 const Sidebar = (props) => {
   const Navigate = useNavigate();
+  const [showNav, setShowNav] = useState();
+
+  const handleSidebar = () => {
+    setShowNav(!showNav);
+  };
+
   return (
     <>
-      <div className={`relative w-72 h-screen bg-gray-800 p-4`}>
+      <div className="menu-container flex items-center justify-between px-8 py-2">
+        <IconGoogle
+          className="cursor-pointer"
+          iconValue="menu"
+          onClick={handleSidebar}
+        />
+      </div>
+      <div
+        className={`sidebar-container fixed z-50 top-0 left-0 w-72 h-screen bg-gray-800 p-4 ${
+          showNav ? "show-nav" : ""
+        } duration-700`}
+      >
         <Title
           className="font-bold text-4xl text-white text-center my-2 cursor-pointer hover:text-gray-400 transition-all"
           Title="GadgetPedia"
